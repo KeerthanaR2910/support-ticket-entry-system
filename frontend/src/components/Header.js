02e31d590781c82957360a04d9d78f59a789a2c3
Keerthana Ramakrishnan <keerthana.r@thoughtworks.com>
2024-01-17 18:08:41 +0530

    Add Header for navigation between pages

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
     return (
-    <h1>Support Tickets Entry System</h1>
+    <div className="m-4 flex flex-col">
+        <p className=" text-3xl font-extrabold text-center ">Support Ticket Entry System</p>
+        <div className="flex flex-col md:flex-row justify-end gap-2 m-4">
+            <Link to="/new-support-agent" className=" bg-neutral-500 p-2 w-fit rounded-md">Create new Support Agent</Link>
+            <Link to="/new-support-ticket" className=" bg-neutral-500 p-2 w-fit rounded-md">Create new Support Ticket</Link>
+            <Link to="/" className=" bg-neutral-500 p-2 w-fit rounded-md">Show Support Tickets</Link>
+        </div>
+    </div>
     )
 }
 

f41fe1079c539c55ee7dd518d0191902836d1996
Keerthana Ramakrishnan <keerthana.r@thoughtworks.com>
2024-01-17 23:20:40 +0530

    Add Success and error modal for create support ticket and did some refactoring

--- frontend/src/pages/CreateSupportTicket/index.js
+++ frontend/src/pages/CreateSupportTicket/index.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import saveSupportTicket from "../../api/saveSupportTicket";
+import Modal from "../../components/Modal";
+import Form from "../../components/Form";
 
 const CreateSupportTicket = () => {
-    const [responseBody, setResponseBody] = useState();
+    const [responseBody, setResponseBody] = useState({});
+    const [saveStatus, setSaveStatus] = useState('None');
 
     const inputChangeHandler = (event) => {
         const {id, value} = event.target
@@ -9,67 +13,66 @@ const CreateSupportTicket = () => {
     }
     
     const onSubmitHandler = async(event) => {
-        event.preventDefault()
-        console.log(responseBody)
+            event.preventDefault()
+            saveSupportTicket(responseBody)
+            .then(() => {
+                setSaveStatus("Success")
+            })
+            .catch(() => {
+                setSaveStatus("Error");
+            })           
     }
+
+    const formFields = [
+        {
+            name:"topic",
+            label: "Topic",
+            type:"input"
+        },
+        {
+            name:"description",
+            label: "Description",
+            type:"input"
+        },
+        {
+            name:"severity",
+            label: "Severity",
+            type:"select",
+            options: ["Low","High","Medium","Urgent"]
+        },
+        {
+            name:"type",
+            label: "Type",
+            type:"input"
+        }
+    ]
+
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center relative">
             <p className=" text-xl font-bold text-center">Create New Support Ticket</p>
-            <form className=" bg-slate-100 w-fit md:w-[50%] rounded-xl p-6 flex justify-center m-4" onSubmit={onSubmitHandler}>
-                <div className="flex flex-col gap-y-4 justify-center w-fit h-full font-sans md:w-2/3">
-                    <div className="flex flex-col h-fit gap-y-2">
-                        <label className=" text-md font-medium">
-                            Topic
-                        </label>
-                        <input 
-                            type="text" 
-                            id="topic" 
-                            required
-                            onChange={(e)=>inputChangeHandler(e)} 
-                            className=" appearance-none bg-slate-300 text-gray-700 p-2 focus:outline-none focus:outline-gray-500 focus:bg-slate-50"/>
-                    </div>
-                    <div className="flex flex-col h-fit gap-y-2">
-                        <label className=" text-md font-medium">
-                            Description
-                        </label>
-                        <input 
-                            type="text" 
-                            id="description" 
-                            required
-                            onChange={(e)=>inputChangeHandler(e)} 
-                            className=" appearance-none bg-slate-300 text-gray-700 p-2 focus:outline-none focus:outline-gray-500 focus:bg-slate-50"/>
-                    </div>
-                    <div className="flex flex-col h-fit gap-y-2">
-                        <label className=" text-md font-medium">
-                            Severity
-                        </label>
-                        <select 
-                            id="Severity" 
-                            required
-                            onChange={(e)=>inputChangeHandler(e)} 
-                            className="w-full bg-slate-300 border border-gray-200 text-gray-700 p-2 focus:outline-none focus:outline-gray-500 focus:bg-slate-50">
-                            <option>Low</option>
-                            <option>Medium</option>
-                            <option>High</option>
-                            <option>Urgent</option>
-                        </select>
-                    </div>
-                    <div className="flex flex-col h-fit gap-y-2">
-                        <label className=" text-md font-medium">
-                        Type
-                        </label>
-                        <input 
-                            type="text" 
-                            id="type" 
-                            required
-                            onChange={(e)=>inputChangeHandler(e)} 
-                            className=" appearance-none bg-slate-300 text-neutral-950 p-2 focus:outline-none focus:outline-gray-500 focus:bg-slate-50"/>
-                    </div>
-                    <div className="m-auto w-[50%]">
-                        <button className="p-2 bg-neutral-600 w-full rounded-lg" type="submit">Create Support Agent</button>
-                    </div>
-                </div>
-            </form>
+            <Form fields={formFields} formData={responseBody} onSubmitHandler={onSubmitHandler} inputChangeHandler={inputChangeHandler}  submitButtonLabel={"Create Support Ticket"}/>
+            {saveStatus === "Success"  &&
+            <Modal message="Success" 
+                handleOkClick={() => {
+                    setSaveStatus("None");
+                    setResponseBody({})
+                }}
+                handleCancelClick= {() => {
+                    setSaveStatus("None");
+                    setResponseBody({});
+                }} />
+            }
+            {saveStatus === "Error"  &&
+                <Modal message="Error"
+                    handleOkClick={() => {
+                        setSaveStatus("None");
+                        setResponseBody({})
+                    }}
+                    handleCancelClick= {() => {
+                        setSaveStatus("None");
+                        setResponseBody({});
+                    }} />
+            }
         </div>
     )
 }

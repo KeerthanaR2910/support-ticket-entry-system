a1a6c775557c281944d7a273cd20321bee3603c4
Keerthana Ramakrishnan <keerthana.r@thoughtworks.com>
2024-01-19 15:47:47 +0530

    Change status while resolving ticket

--- backend/src/handlers/resolveTicket.js
+++ backend/src/handlers/resolveTicket.js
@@ -3,7 +3,7 @@ import {Ticket} from "../models/ticket.js";
 const resolveTicketHandler =  async (request, response)  => {
     try {
         const ticketId = request.params['id']
-        const ticket = await Ticket.findByIdAndUpdate(ticketId, {"resolvedOn": Date.now()})
+        const ticket = await Ticket.findByIdAndUpdate(ticketId, {"resolvedOn": Date.now(),status: "Resolved"})
         return response.status(200).json(ticket);
     }
     catch (error) {

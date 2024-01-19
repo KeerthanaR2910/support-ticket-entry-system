b726cf269e4aee5ebe4186f583d652a9475bf4df
Keerthana Ramakrishnan <keerthana.r@thoughtworks.com>
2024-01-19 13:38:44 +0530

    Update collection in mongo db

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,3 +1,3 @@
 export const PORT = 8888
 
-export const MONGO_DB_URL ='mongodb+srv://admin:admin@demo.kxy6bpg.mongodb.net/demo-collection?retryWrites=true&w=majority'
+export const MONGO_DB_URL ='mongodb+srv://admin:admin@demo.kxy6bpg.mongodb.net/support-ticket-system?retryWrites=true&w=majority'
